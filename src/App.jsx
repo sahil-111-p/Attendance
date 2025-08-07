62270dd03a0669d6683daf4367cfd0300a206387
sahil-111-p <sahilpanchigar@gmail.com>
2025-08-07 16:27:20 +0530

    restructured

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
-import React from "react";
-import "./App.css";
-import Card from "./components/Card";
-import Previous from "./components/Previous";
-import Navbar from "./components/Navbar";
-import { LoginPage } from "./components/LoginPage";
+// import React from "react";
+// import "./App.css";
+// import Card from "./components/Card";
+// import Previous from "./components/Previous";
+// import Navbar from "./components/Navbar";
+// import { LoginPage } from "./components/LoginPage";
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <>
+//       <LoginPage/>
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+
+import './App.css'
+import TeacherInfo from './components/pages/user_side/TeacherInfo';
+import { motion, AnimatePresence } from "framer-motion";
+import AnimationPage from './components/pages/user_side/AnimationPage'
+import React, { useEffect, useRef, useState } from 'react';
+// import { Html5Qrcode, Html5QrcodeScanner } from 'html5-qrcode';
+import Qrcode from './components/pages/user_side/Qrcode'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Logout from './components/pages/user_side/Logout'
+import Dashboad from './components/pages/user_side/Dashboad';
+import { LoginPage } from './components/pages/user_side/LoginPage'
+
+
+function App() {
+  const location = useLocation();
   return (
     <>
-      <LoginPage/>
+    <AnimatePresence mode='wait'>
+    <Routes location={location} key={location.pathname}>
+      <Route path='/' element={<LoginPage/>}/>
+      <Route path='/Teacherinfo' element={<TeacherInfo/>}/>
+      <Route path='/scanQR' element={<Qrcode/>} />
+      <Route path='/' element={<TeacherInfo/>} />
+      <Route path='/Complete' element={<AnimationPage/>} />
+      <Route path='/ShowLogOut' element={<Logout/>} />
+      <Route path='/Dashboard' element={<Dashboad/>} />
+    </Routes>
+    </AnimatePresence>
     </>
   );
-};
+}
 
-export default App;
+export default App
--- src/components/pages/user_side/LoginPage.jsx
+++ src/components/pages/user_side/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 export const LoginPage = () => {
     const navigate = useNavigate();
