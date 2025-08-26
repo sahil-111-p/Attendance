14bda5651b043ae65334883d029205ee49950a1f
sahil-111-p <sahilpanchigar@gmail.com>
2025-08-27 00:18:58 +0530

    file edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
-import TeacherInfo from "./components/pages/user_side/TeacherInfo";
+import TeacherInfo from "./components/user_side/TeacherInfo";
 import { motion, AnimatePresence } from "framer-motion";
-import AnimationPage from "./components/pages/user_side/AnimationPage";
+import AnimationPage from "./components/user_side/AnimationPage";
 import React, { useEffect, useRef, useState } from "react";
 // import { Html5Qrcode, Html5QrcodeScanner } from 'html5-qrcode';
-import Qrcode from "./components/pages/user_side/Qrcode";
+import Qrcode from "./components/user_side/Qrcode";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Logout from "./components/pages/user_side/LogoutPage";
-import Dashboad from "./components/pages/user_side/Dashboad";
-import { LoginPage } from "./components/pages/user_side/LoginPage";
-import LoginPage_G from "./components/pages/Guard_side/LoginPage_G";
-import QRGenrator_G from "./components/pages/Guard_side/QRGenrator_G";
+import Logout from "./components/user_side/LogoutPage";
+import Dashboad from "./components/user_side/Dashboad";
+import { LoginPage } from "./components/user_side/LoginPage";
+import LoginPage_G from "./components/Guard_side/LoginPage_G";
+import QRGenrator_G from "./components/Guard_side/QRGenrator_G";
 // import { QRCodeSVG } from 'qrcode.react';
 
 function App() {
